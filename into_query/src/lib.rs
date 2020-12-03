7830e299dcd1ddab100aa9bf1325567c40f57229
Charlie Thomson <charliecthomson@gmail.com>
2020-12-03 01:00:09 -0500

    add docs

--- into_query/src/lib.rs
+++ into_query/src/lib.rs
@@ -1,8 +1,10 @@
-
+/// Provides the `into_query` function, which converts the type into a select statement filtering by present fields.
+/// T is the table the select statement will filter by.
 pub trait IntoQuery<T>
 where
     T: diesel::Table + diesel::query_builder::AsQuery,
     T::Query: diesel::query_dsl::methods::BoxedDsl<'static, diesel::mysql::Mysql>,
 {
+    /// Convert `self` into a query
     fn into_query(self) -> diesel::helper_types::IntoBoxed<'static, T, diesel::mysql::Mysql>;
 }
